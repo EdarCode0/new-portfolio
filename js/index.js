5b29d9443fab988529abec308960151419939692
Edar Stojku <kuroki2201@outlook.com>
2023-08-23 00:10:22 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 function closeNavbar() {
-    const navbarCollapse = document.getElementById('navbarSupportedContent');
-    navbarCollapse.classList.remove('show');
-  }
+  const navbarCollapse = document.getElementById('navbarSupportedContent');
+  navbarCollapse.classList.remove('show');
+}
 
-const navEl = document.querySelector('.navbar');
+var navbar = document.getElementById("nav1");
+var navbarToggler = document.querySelector(".navbar-toggler");
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 56) {
-    navEl.classList.add('navbar-scrolled');
-  } else if (window.scrollY < 56) {
-    navEl.classList.remove('navbar-scrolled');
+// Function to update the navbar color
+function updateNavbarColor() {
+  if (window.scrollY > 0 || (navbarToggler.getAttribute("aria-expanded") === "true")) {
+    navbar.style.backgroundColor = "#102634"; // Change to the desired color
+  } else {
+    navbar.style.backgroundColor = "transparent"; // Reset to transparent
   }
+}
+
+// Listen for scroll events
+window.addEventListener("scroll", function () {
+  updateNavbarColor();
 });
 
-
+// Listen for Bootstrap collapsible events
+navbarToggler.addEventListener("click", function () {
+  updateNavbarColor();
+});
