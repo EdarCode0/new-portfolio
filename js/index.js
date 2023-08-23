f765d182aaa9a22af560cad3be96d18831d6e11f
Edar Stojku <kuroki2201@outlook.com>
2023-08-23 17:27:39 +0200

    update

--- js/index.js
+++ js/index.js
@@ -24,3 +24,51 @@ window.addEventListener("scroll", function () {
 navbarToggler.addEventListener("click", function () {
   updateNavbarColor();
 });
+
+// Function to start the counter animation
+function animateCounters() {
+  $('.counter-count').each(function () {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 5000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+}
+
+// Attach scroll event listener to trigger animation when user scrolls to reviews section
+$(window).on('scroll', function () {
+  var reviewsSection = $('.reviews');
+  var windowHeight = $(window).height();
+  var scrollTop = $(window).scrollTop();
+  var sectionOffset = reviewsSection.offset().top;
+
+  // Adjust the scroll offset based on your needs, you can add some buffer to start the animation before reaching the section
+  var scrollOffset = windowHeight - (windowHeight / 4);
+
+  if (scrollTop + scrollOffset > sectionOffset) {
+    animateCounters();
+    // Remove the event listener after the animation is triggered to avoid unnecessary re-triggers
+    $(window).off('scroll');
+  }
+});
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+      // Get the position of the tech section
+      const techSectionPosition = $('#technologies').offset().top;
+      
+      // Get the current scroll position
+      const scrollPosition = $(window).scrollTop() + $(window).height();
+      
+      // Check if the tech section is in the viewport
+      if (scrollPosition > techSectionPosition) {
+          $('.info-card').addClass('smooth-transition show');
+      }
+  });
+});
